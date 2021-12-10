2c176610e23a3fb4229b438b2555eab16eaa07ce
Peter Sherman <pfnsec@gmail.com>
2021-12-10 23:08:00 +0000

    Enable multistage images with install_hook

--- batik/commands/build.py
+++ batik/commands/build.py
@@ -1,7 +1,10 @@
 import requests
-import docker
 import os
+import tempfile
 import pathlib
+from jinja2 import Template
+import asyncio
+import subprocess
 
 from .base import Base
 
@@ -13,31 +16,46 @@ class Build(Base):
     """Build a container from a manifest"""
 
     def run(self):
-        client = docker.from_env()
         mfst = manifest.Manifest()
         mfst.parse_file()
         alias = mfst.alias
 
         registry = batik.env.get('image_registry')
 
-        dockerfile = os.path.join(
+        template_base = '../templates/docker'
+        template_file = os.path.join(
             pathlib.Path(__file__).parent.resolve(),
-            '../templates/docker/Dockerfile.default'
+            template_base,
+            'Dockerfile.default'
         )
 
-        print(f"BUILD {registry}/{alias}")
+        install_hook = 'pip install -r requirements.txt'
 
-        img, lines = client.images.build(
-            path = '.',
-            dockerfile = dockerfile,
-            tag = f"{registry}/{alias}"
-        )
-        for line in lines:
-            if 'stream' in line:
-                print(line['stream'])
+        with open(template_file) as f:
+            template = Template(f.read())
+        rendered = template.render(install_hook=install_hook)
+
+        tmp = tempfile.NamedTemporaryFile(mode='w+', delete=False)
+        try:
+            print(tmp.name)
+            tmp.write(rendered)
+            tmp.seek(0)
+            p = subprocess.Popen([
+                'docker', 'build',
+                '-f', tmp.name,
+                '-t', f"{registry}/{alias}",
+                '.',
+            ], shell=False)
+            p.wait()
+
+        finally:
+            tmp.close()
+            os.unlink(tmp.name)
         
         if(self.options['--push']):
-            res = client.images.push(f"{registry}/{alias}")
-            print(res)
+            subprocess.Popen([
+                'docker', 'push',
+                f"{registry}/{alias}",
+            ], shell=False)
 
         
