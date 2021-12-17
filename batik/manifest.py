544ef7fc2704bdc23158ccef85c2ed63e9df15db
Peter Sherman <pfnsec@gmail.com>
2021-12-17 00:02:27 +0000

    Manifest refactor for future backends

--- batik/manifest.py
+++ batik/manifest.py
@@ -47,23 +47,16 @@ def func_by_path(name, *args, **kwargs):
     return fn
 
 
-class Endpoint:
-    def __init__(self, input_type):
-        self.layers = []
-        self.input_type = input_type
-    
-    def add_layer(self, layer):
-        self.layers.append(layer)
-
 
 class Layer:
     def __init__(self, manifest, path, args):
+        self.manifest = manifest
         self.pass_manifest = False
         self.actor = None
         self.args = args 
 
         if path[0] == "$":
-            # match $Inst.method , for example
+            # match $Actor.method , for example
             actor, fn = path[1:].split('.')
             self.actor = manifest.get_actor(actor).cl
             self.fn = getattr(self.actor, fn)
@@ -75,15 +68,51 @@ class Layer:
         if 'batik_manifest' in sig.parameters:
             self.pass_manifest = True
 
+    def run(self, x):
+        if self.pass_manifest:
+            return self.fn(x, batik_manifest=self.manifest)
+        else:
+            return self.fn(x)
+
 
 class Actor:
     # Actor's layer_args are contained within the underlying actor class
     def __init__(self, class_path, args):
-        self.cl = actor_by_path(class_path)(layer_args=args)
+        if args is None:
+            self.cl = actor_by_path(class_path)()
+        else:
+            self.cl = actor_by_path(class_path)(**args)
 
+
+class Endpoint:
+    def __init__(self):
+        self.layers = []
+    
+    def add_layer(self, layer: Layer):
+        self.layers.append(layer)
+
+    # Cast an input variable to the type annotated by
+    # the first layer of the endpoint.
+    def cast(self, x: Any):
+        fn = self.layers[0].fn
+        sig = inspect.signature(fn)
+        params = list(sig.parameters.values())
+        annotation = params[0].annotation
+        if annotation == inspect._empty:
+            return x
+        else:
+            return annotation(x)
+
+    def run(self, x):
+        y = x
+        for layer in self.layers:
+            y = layer.run(y)
+        return y
 
 class Daemon:
-    def __init__(self, manifest, path, args, endpoint, steps):
+    def __init__(self, name, manifest, path, args, endpoint, steps):
+        self.name = name
+        self.manifest = manifest
         self.pass_manifest = False
         self.actor = None
         self.args = args            
@@ -91,7 +120,7 @@ class Daemon:
         self.layers = []
 
         if path[0] == "$":
-            # match $Inst.method , for example
+            # match $Actor.method , for example
             actor, fn = path[1:].split('.')
             self.actor = manifest.get_actor(actor).cl
             self.fn = getattr(self.actor, fn)
@@ -106,13 +135,33 @@ class Daemon:
                 layer = Layer(manifest, path, args)
                 self.layers.append(layer)
 
+    def run(self):
+        # For every value produced by the generator fn()...
+        for res in self.fn(**self.args):
+            trace = Trace(f'daemon/{self.name}')
+            # Run it through its layers...
+            for layer in self.layers:
+                res = layer.run(res)
+            # ... and pass the result to its endpoint, if it exists
+            if self.endpoint is not None:
+                ep = self.manifest.get_endpoint(self.endpoint)
+                ep.run(res)
+
+# Trace a call through the compute graph.
+class Trace:
+    def __init__(self, source):
+        self.source = source
+        self.path = []
+
+    def trace(self, ):
+        pass
 
 class Manifest:
     def __init__(self):
         self.alias = ""
         self.actors = {}
         self.endpoints = {}
-        self.daemons = []
+        self.daemons = {}
     
     def parse_file(self):
         with open("./batik.yaml") as manifest:
@@ -121,24 +170,24 @@ class Manifest:
             return self.parse(mfst)
 
     def parse(self, mfst):
-
         for actor in mfst.get('actors') or []:
             name       = actor["name"]
             class_path = actor["class"]
             args       = actor.get("args")
 
-            self.add_actor(Actor(class_path, args), name)
+            act = Actor(class_path, args)
 
+            self.add_actor(act, name)
 
         for endpoint in mfst.get('endpoints') or []:
             name = endpoint['name']
             input_type = endpoint.get("input_type") or 'str'
 
-            ep = Endpoint(input_type)
+            ep = Endpoint()
 
             for step in endpoint['steps']:
-                path = step['name']
-                args = step.get('args')
+                path  = step['name']
+                args  = step.get('args')
                 layer = Layer(self, path, args)
                 ep.add_layer(layer)
 
@@ -146,18 +195,16 @@ class Manifest:
 
 
         for daemon in mfst.get('daemons') or []:
+            name      = daemon["name"]
             generator = daemon["generator"]
-            args = daemon.get("args")
-            endpoint = daemon.get("endpoint")
-            steps = daemon.get("steps")
+            args      = daemon.get("args")
+            endpoint  = daemon.get("endpoint")
+            steps     = daemon.get("steps")
 
-            dm = Daemon(self, generator, args, endpoint, steps)
+            dm = Daemon(name, self, generator, args, endpoint, steps)
 
-            self.add_daemon(dm)
+            self.add_daemon(dm, name)
 
-    
-    def add_daemon(self, daemon: Daemon):
-        self.daemons.append(daemon)
 
     def add_actor(self, actor: Actor, name: str):
         self.actors[name] = actor
@@ -167,19 +214,17 @@ class Manifest:
 
     def add_endpoint(self, endpoint: Endpoint, name: str):
         self.endpoints[name] = endpoint
-    
 
-    def daemon_thread(self, daemon):
-        layers = daemon.layers or []
-        endpoint = daemon.endpoint
-        args = daemon.args
-        # For every value produced by the generator fn()...
-        for res in daemon.fn(**args):
-            # Run it through its layers...
-            res = self.run_layer_set(layers, res)
-            # ... and pass the result to its endpoint, if it exists
-            if endpoint is not None:
-                self.run_endpoint(endpoint, res)
+    def get_endpoint(self, name: str):
+        return self.endpoints[name]
+    
+    def add_daemon(self, daemon: Daemon, name: str):
+        self.daemons[name] = daemon
+
+    def get_daemon(self, name: str):
+        return self.daemons[name]
+
+
 
 
     def spawn_daemons(self):
@@ -211,11 +256,13 @@ class Manifest:
         return res
 
     
-    def run_endpoint(self, name: str, payload: Any):
+    def run_endpoint(self, name: str, payload: Any, cast=False):
 
         ep = self.endpoints[name]
 
-        res = self.run_layer_set(ep.layers, payload)
+        payload = ep.cast(payload)
+
+        res = ep.run(payload)
 
         return res
     
