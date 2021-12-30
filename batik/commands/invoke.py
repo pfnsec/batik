646d14d96c413a6b2d2a569a9ff5dd177e1ac219
Peter Sherman <pfnsec@gmail.com>
2021-12-30 14:55:30 +0000

    Websocket-aware 'invoke' command

--- batik/commands/invoke.py
+++ batik/commands/invoke.py
@@ -1,4 +1,4 @@
-import requests
+import aiohttp
 import json
 from .base import Base
 
@@ -6,17 +6,32 @@ from .base import Base
 class Invoke(Base):
     """Invoke endpoints"""
 
-    def run(self):
+    async def run(self):
         endpoint = self.options["<endpoint>"]
 
         use_json = self.options.get("--json") or None
         if use_json:
             with open(self.options["<file>"]) as json_file:
                 payload = json.load(json_file)
-            r = requests.post(f"http://localhost:5678/endpoint/{endpoint}", json = payload)
-
         else:
             payload = self.options.get("<payload>") or None
-            r = requests.post(f"http://localhost:5678/endpoint/{endpoint}", json = payload)
 
-        print(str(r.content))
+        data = {
+            'cmd': 'invoke',
+            'endpoint': endpoint,
+            'payload': payload,
+        }
+
+        async with aiohttp.ClientSession() as session:
+            async with session.ws_connect('http://localhost:8086/ws/') as ws:
+                await ws.send_json(data)
+
+                async for msg in ws:
+                    if msg.type == aiohttp.WSMsgType.TEXT:
+                        if msg.data == 'goodbye':
+                            await ws.close()
+                            break
+                        else:
+                            print(msg.json())
+                    elif msg.type == aiohttp.WSMsgType.ERROR:
+                        break
